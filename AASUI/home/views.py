d06b06e4ee803fcfeb2ff55bbbe3a2a822690471
haikentcode <hiteshnitj16@gmail.com>
2016-04-27 03:46:17 +0530

    change view.py for clint side capture

--- AASUI/home/views.py
+++ AASUI/home/views.py
@@ -72,62 +72,93 @@ def history(request,year=0,month=0,day=0):
     return render(request,'home/history.html',{'attendanceList':attendanceList,'user':teacher})
 
 
+def getimageslables(course,branch,batch):
+            students=Student.objects.filter(course__iexact=course,branch__iexact=branch,batch__iexact=batch) # __iexact ignore case
+            images=[]
+            lables=[]
+            for student in students:
+                for image in student.imagedata_set.all():
+                    lables.append(int(student.rollNumber))
+                    img='media/%s' % image
+                    img=cv2.imread(img)
+                    img=cv2.resize(img,(600,600),interpolation=cv2.INTER_AREA)
+                    img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+                    images.append(img)
+            return images ,lables
+
+
+def getpredictABC(r1,r2,r3,frame):
+    face=obj.Face(1.6,5,20,20)
+    faces=face.getFaces(frame)
+    print "No of faces found",len(faces)
+    abclist=[]
+    for f in faces:
+           simg=cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
+           simg=cv2.resize(simg,(600,600),interpolation=cv2.INTER_AREA)
+           a=r1.getLable(simg)
+           b=r2.getLable(simg)
+           c=r3.getLable(simg)
+           abclist.append(( a,b,c))
+    return abclist
+
+
+
+def getr1r2r3(course,branch,batch):
+      images,lables=getimageslables(course,branch,batch)
+      print images,lables
+      r1 = rec.Recognizer()
+      r2 = rec.Recognizer()
+      r3 = rec.Recognizer()
+      r1.train(images,lables,0)
+      r2.train(images,lables,1)
+      r3.train(images,lables,2)
+      return r1,r2,r3
+
+def setBranchCourseBatch(request):
+    course=request.POST.get('course')
+    branch=request.POST.get('branch')
+    batch=request.POST.get('batch')
+    request.session['course']=course
+    request.session['branch']=branch
+    request.session['batch']=batch
+
+def getBranchCourseBatch(request):
+    course=request.session.get('course')
+    branch=request.session.get('branch')
+    batch=request.session.get('batch')
+    return branch,course,batch
+
 def startcapturing(request):
-      path=cpath("/home/haikent/Desktop/fyp/digitalEye/AASUI/media/media/dic.mp4")
-      print path
+      #this is way to read video frame
+      #path=cpath("/home/haikent/Desktop/fyp/digitalEye/AASUI/media/media/dic.mp4")
+      #print path
+      #cap=cv2.VideoCapture(path)
+      #-------------------*------------------------------------//
+      """
       cap=cv2.VideoCapture(0) #"http://10.42.0.41:8080/video"
       course=request.POST.get('course')
       branch=request.POST.get('branch')
       batch=request.POST.get('batch')
-      students=Student.objects.filter(course__iexact=course,branch__iexact=branch,batch__iexact=batch) # __iexact ignore case
 
-      images=[]
-      lables=[]
-      for student in students:
-          for image in student.imagedata_set.all():
-              lables.append(int(student.rollNumber))
-              img='media/%s' % image
-              img=cv2.imread(img)
-              img=cv2.resize(img,(600,600),interpolation=cv2.INTER_AREA)
-              img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-              images.append(img)
-      print images
-      print lables
+      r1,r2,r3=getr1r2r3(course,branch,batch)
 
-      recog = rec.Recognizer()
-      recog1 = rec.Recognizer()
-      recog2 = rec.Recognizer()
-      recog.train(images,lables,0)
-      recog1.train(images,lables,1)
-      recog2.train(images,lables,2)
-      face=obj.Face(1.6,5,20,20)
       hk=True
       while(True):
          ret,frame=cap.read()
          if not ret: continue
          cv2.imshow('frame',frame)
-         faces=face.getFaces(frame)
-         print "No of faces found",len(faces)
-         for f in faces:
-                simg=cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
-                simg=cv2.resize(simg,(600,600),interpolation=cv2.INTER_AREA)
-                a=recog.getLable(simg)
-                b=recog1.getLable(simg)
-                c=recog2.getLable(simg)
-                print "predictedLable:",a,b,c
-                if False: #c[1] <  24000
-                    cv2.imshow(str(b[0])+":"+str(b[1])+","+str(c[0])+":"+str(c[1]),simg)
-                if hk:
-                   log=Log()
-                   log.text= "predictedLable: %s %s %s" % (a,b,c)
-                   log.save()
-
+         print getpredictABC(r1,r2,r3,frame)
          if cv2.waitKey(1)& 0xFF==ord('q'):
                  break
       cv2.destroyAllWindows()
 
       print "complete task"
       return HttpResponse("ho gya")
+      """
+      setBranchCourseBatch(request)
+
+
+
 
 
 def handle_uploaded_file(f,name):
@@ -185,5 +216,5 @@ def webcamimage(request):
        if request.FILES:
            image=request.FILES.get('webcam')
            image=get_uploaded_image(image,teacher.emailId)
-           
+           print image ,"notkfdfd"
        return HttpResponse("done")
